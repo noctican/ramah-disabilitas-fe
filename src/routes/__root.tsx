a1b62c1d93e97d84014b28cd066c3f0a68248b52
Candra Andika <candrandika999@gmail.com>
2026-01-14 11:37:42 +0700

    mod: change logic for hearing disability

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,5 @@
+import { HEARING_DISABILITY } from '@/data/const/disability'
+import { useAuthStore } from '@/data/store/auth_store'
 import { useVoiceStore } from '@/data/store/voice_store'
 import { useVoiceAssistant } from '@/hooks/use-voice-assistant'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
@@ -10,9 +12,9 @@ import { Toaster } from 'sonner'
 export const Route = createRootRoute({
   component: () => {
     useVoiceAssistant()
-    const disablePerson = true
     const [isFirst, setIsFirst] = useState(true)
     const { setIsActive, isActive, isListening, lastTranscript, speak } = useVoiceStore()
+    const { disability } = useAuthStore()
 
     const startAssistant = () => {
       setIsActive(true)
@@ -20,19 +22,19 @@ export const Route = createRootRoute({
     }
   
     useEffect(() => {
-      if(disablePerson && !isActive && isFirst) {
+      if(disability?.some(d => d == HEARING_DISABILITY) && !isActive && isFirst) {
         setIsFirst(false)
-        speak('woooi, rip, push F E')
+        speak('silahkan ketuk layar terlebih dahulu untuk memu')
       }
-    }, [disablePerson, isActive, isFirst])
+    }, [disability, isActive, isFirst])
     return (
       <>
         <Outlet />
         <Toaster richColors position="top-right" />
 
-        {/* {disablePerson && !isActive && <button className='absolute left-0 right-0 bottom-0 top-0 opacity-0 z-9999' onClick={startAssistant}></button>}
+        {disability?.some(d => d == HEARING_DISABILITY) && !isActive && <button className='absolute left-0 right-0 bottom-0 top-0 opacity-0 z-9999' onClick={startAssistant}></button>}
 
-        {disablePerson &&
+        {disability?.some(d => d == HEARING_DISABILITY) &&
           <div className='absolute left-0 right-0 bottom-0 z-10'>
             <div className={`p-4 text-white flex justify-between items-center ${isActive ? 'bg-lime-500' : 'bg-yellow-500'}`}>
               <div>
@@ -42,7 +44,7 @@ export const Route = createRootRoute({
               </div>
             </div>
           </div>
-        } */}
+        }
         {/* <TanStackDevtools
           config={{
             position: 'bottom-right',
